cf8beea7e48a557aac82d42a6f842948e1b15fee
Avinash <avinashsrivastav196@gmail.com>
2025-01-11 15:22:55 +0530

    Portfolio 🎉

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,9 @@ import Contact from './components/Contact';
 import Experience from './components/Experience';
 import Home from './components/Home';
 import Navbar from './components/NavBar';
+import ParticlesBackground from './components/ParticlesBackground';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
-import ParticlesBackground from './components/ParticlesBackground';
-import Scene3D from './components/Scene3D';
 
 const App = () => {
   return (
